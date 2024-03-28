05b1d74ab85b1d25782c54f2b7f4e8a763a6dbeb
san9nelox <bedinpasha@mail.ru>
2024-03-28 11:18:37 +0500

    Add the default message

--- PlayerDatabase/Exceptions/NotEntryException.cs
+++ PlayerDatabase/Exceptions/NotEntryException.cs
@@ -2,7 +2,7 @@
 
 public class NotEntryException : Exception
 {
-    public NotEntryException(string message) : base(message)
+    public NotEntryException(string message = "The entry is not in the database") : base(message)
     {
     }
 }
