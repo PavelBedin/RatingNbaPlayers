ab9288b08b86eab16fd88daaea7dc09ca4366691
san9nelox <bedinpasha@mail.ru>
2024-03-29 18:54:12 +0500

    Add default constructor

--- PlayerDatabase/AdvancedStatistics.cs
+++ PlayerDatabase/AdvancedStatistics.cs
@@ -1,20 +1,26 @@
-﻿namespace DataBase;
-
-public record AdvancedStatistics(
-    int PlayerId,
-    double OffensiveRating = 0,
-    double DefensiveRating = 0,
-    double NetRating = 0,
-    double AssistPercentage = 0,
-    double AssistToTurnoverRatio = 0,
-    double AssistRatio = 0,
-    double OffensiveReboundsPercentage = 0,
-    double DefensiveReboundsPercentage = 0,
-    double ReboundsPercentage = 0,
-    double TurnoverRatio = 0,
-    double EffectiveFieldGoalsPercentage = 0,
-    double TrueShootingPercentage = 0,
-    double UsagePercentage = 0,
-    double Pace = 0,
-    double PlayerImpactEstimate = 0
-);
+﻿namespace DataBase
+{
+    public record AdvancedStatistics(
+        int PlayerId, // Player identifier
+        double OFFRTG = 0, // Offensive efficiency rating
+        double DEFRTG = 0, // Defensive efficiency rating
+        double NETRTG = 0, // Net rating
+        double ASTP = 0, // Percentage of assists
+        double ASTTO = 0, // Assist to turnover ratio
+        double ASTR = 0, // Assist ratio
+        double OREBP = 0, // Offensive rebounds percentage
+        double DREBP = 0, // Defensive rebounds percentage
+        double REBP = 0, // Rebounds percentage
+        double TOR = 0, // Turnover ratio
+        double EFGP = 0, // Effective field goals percentage
+        double TSP = 0, // True shooting percentage
+        double USGP = 0, // Usage percentage
+        double PACE = 0, // Pace
+        double PIE = 0 // Player impact estimate
+    )
+    {
+        public AdvancedStatistics() : this(0)
+        {
+        }
+    }
+}
