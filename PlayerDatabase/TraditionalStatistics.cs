ac7301eda60a52f23c1455afbcd639c1cd04c1f8
san9nelox <bedinpasha@mail.ru>
2024-03-29 15:09:27 +0500

    Add ctor

--- PlayerDatabase/TraditionalStatistics.cs
+++ PlayerDatabase/TraditionalStatistics.cs
@@ -1,28 +1,41 @@
-﻿namespace DataBase;
-
-public record TraditionalStatistics(
-    int PlayerId,
-    int GamesPlayed = 0,
-    double MinutesPlayed = 0,
-    double PointsPerGame = 0,
-    double FieldGoalsMade = 0,
-    double FieldGoalsAttempted = 0,
-    double FieldGoalsPercentage = 0,
-    double ThreePointFieldGoalsMade = 0,
-    double ThreePointFieldGoalsAttempted = 0,
-    double ThreePointFieldGoalsPercentage = 0,
-    double FreeThrowsMade = 0,
-    double FreeThrowsAttempted = 0,
-    double FreeThrowsPercentage = 0,
-    double OffensiveRebounds = 0,
-    double DefensiveRebounds = 0,
-    double TotalRebounds = 0,
-    double Assists = 0,
-    double Turnovers = 0,
-    double Steals = 0,
-    double Blocks = 0,
-    double PersonalFouls = 0,
-    int DoubleDoubles = 0,
-    int TripleDoubles = 0,
-    double PlusMinus = 0
-);
+﻿namespace DataBase
+{
+    public record TraditionalStatistics(
+        int PlayerId,
+        int GamesPlayed = 0,
+        double MinutesPlayed = 0.0,
+        double PointsPerGame = 0.0,
+        double FieldGoalsMade = 0.0,
+        double FieldGoalsAttempted = 0.0,
+        double FieldGoalsPercentage = 0.0,
+        double ThreePointFieldGoalsMade = 0.0,
+        double ThreePointFieldGoalsAttempted = 0.0,
+        double ThreePointFieldGoalsPercentage = 0.0,
+        double FreeThrowsMade = 0.0,
+        double FreeThrowsAttempted = 0.0,
+        double FreeThrowsPercentage = 0.0,
+        double OffensiveRebounds = 0.0,
+        double DefensiveRebounds = 0.0,
+        double TotalRebounds = 0.0,
+        double Assists = 0.0,
+        double Turnovers = 0.0,
+        double Steals = 0.0,
+        double Blocks = 0.0,
+        double PersonalFouls = 0.0,
+        int DoubleDoubles = 0,
+        int TripleDoubles = 0,
+        double PlusMinus = 0.0
+    )
+    {
+        public TraditionalStatistics(int playerId, IReadOnlyList<object> values)
+            : this(playerId)
+        {
+            if (values.Count <= 0) return;
+            for (var i = 1; i < values.Count && i < GetType().GetProperties().Length - 1; i++)
+            {
+                var property = GetType().GetProperties()[i];
+                property.SetValue(this, Convert.ChangeType(values[i], property.PropertyType));
+            }
+        }
+    }
+}
