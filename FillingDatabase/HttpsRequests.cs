9fb8e2ec65209d41f0123b05372dd1a7d3a47988
san9nelox <bedinpasha@mail.ru>
2024-04-02 15:27:15 +0500

    Add FillingDatabase

---
+++ FillingDatabase/HttpsRequests.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+
+class HttpRequests
+{
+    public async void Main()
+    {
+        const string url = "https://www.basketball-reference.com/leagues/NBA_2024_per_game.html";
+
+        using var client = new HttpClient();
+        try
+        {
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+                var playerNameNodes = doc.DocumentNode.SelectNodes("//td[@data-stat='player']");
+                if (playerNameNodes != null)
+                {
+                    foreach (var node in playerNameNodes)
+                    {
+                        var playerName = node.InnerText;
+                        Console.WriteLine(playerName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Информация о текущих играющих игроках не найдена.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
