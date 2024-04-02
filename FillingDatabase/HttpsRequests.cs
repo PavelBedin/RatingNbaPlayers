a9c64eacd37b18868081577e9891e2303c9f8340
san9nelox <bedinpasha@mail.ru>
2024-04-02 16:30:41 +0500

    Try to do request

--- FillingDatabase/HttpsRequests.cs
+++ FillingDatabase/HttpsRequests.cs
@@ -1,43 +1,45 @@
 ﻿using HtmlAgilityPack;
 
-class HttpRequests
+class HttpsRequests
 {
-    public async void Main()
+    public static async Task GetTraditionalStatistics()
     {
         const string url = "https://www.basketball-reference.com/leagues/NBA_2024_per_game.html";
 
         using var client = new HttpClient();
-        try
-        {
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var html = await response.Content.ReadAsStringAsync();
 
-                var doc = new HtmlDocument();
-                doc.LoadHtml(html);
-                var playerNameNodes = doc.DocumentNode.SelectNodes("//td[@data-stat='player']");
-                if (playerNameNodes != null)
-                {
-                    foreach (var node in playerNameNodes)
-                    {
-                        var playerName = node.InnerText;
-                        Console.WriteLine(playerName);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Информация о текущих играющих игроках не найдена.");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Ошибка: {response.StatusCode}");
-            }
-        }
-        catch (Exception ex)
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
         {
-            Console.WriteLine($"Ошибка: {ex.Message}");
+            var html = await response.Content.ReadAsStringAsync();
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+
+            var playerNodes =
+                doc.DocumentNode.SelectNodes("//tr[@class='full_table' or @class='italic_text partial_table']");
+
+            if (playerNodes != null)
+            {
+                foreach (var node in playerNodes)
+                {
+                    var playerName = node.SelectSingleNode(".//td[@data-stat='player']/a")?.InnerText;
+
+                    var playerStats = node.SelectNodes(".//td[@data-stat!='player']")
+                        .Select(td => td.InnerText)
+                        .ToList();
+
+                    Console.WriteLine($"Имя: {playerName}");
+                    Console.WriteLine("Статистика:");
+                    foreach (var stat in playerStats)
+                    {
+                        Console.WriteLine(stat);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
