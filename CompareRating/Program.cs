4c698acf6069df56e8dbec27ecd87a9659e5a65b
san9nelox <bedinpasha@mail.ru>
2024-05-24 13:51:26 +0500

    Add CompareRating

---
+++ CompareRating/Program.cs
@@ -0,0 +1,11 @@
+﻿using PlayerDatabase;
+
+class Program
+{
+    public static void Main()
+    {
+        var db = new Database();
+        var cr = db.GetAllComparisonOfRating();
+        Console.WriteLine(string.Join(' ', cr));
+    }
+}
